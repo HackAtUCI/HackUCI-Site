e9a02d975f5b301da26faab7373a6c4d022b61fd
Taesung Hwang <taesungh@uci.edu>
2022-12-18 14:46:50 -0800

    Install Bootstrap, React Bootstrap, and Sass
    - Set up Bootstrap styles for overriding variables and using mixins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Footer, Navigation } from "components";
 import type { AppProps } from "next/app";
+import "styles/bootstrap.scss";
 import "styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
