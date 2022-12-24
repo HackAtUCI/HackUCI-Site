ae008da52ae25a866182c9a73c7436dfc11e0398
Riley Wong <rileysw@uci.edu>
2022-12-23 17:38:41 -0800

    setup sendgrid integration for sending personalized templated emails

---
+++ src/api/sendgrid/sendgrid_email.py
@@ -0,0 +1,44 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Email, Personalization
+
+
+def send_email(template_id: str, sender_email: str, receiver_data: dict or list, send_to_multiple: bool = False) -> None:
+    '''
+    Send a personalized templated email to one or multiple receivers via SendGrid
+    '''
+    load_dotenv()
+    sendgrid_api_key = os.environ.get('SENDGRID_API_KEY')
+
+    try:
+        email_message = Mail()
+
+        if send_to_multiple:
+            if type(receiver_data) != list:
+                raise TypeError(f'receiver_data expects {list}. Passed object has type {type(receiver_data)}')
+            else:
+                for r in receiver_data:
+                    p = Personalization()
+                    p.add_to(Email(email=r['email'], dynamic_template_data=r))
+                    email_message.add_personalization(p)
+        else:
+            if type(receiver_data) != dict:
+                raise TypeError(f'receiver_data expects {dict}. Passed object has type {type(receiver_data)}')
+            else:
+                p = Personalization()
+                p.add_to(Email(email=receiver_data['email'], dynamic_template_data=receiver_data))
+                email_message.add_personalization(p)
+    
+        email_message.from_email = sender_email
+        email_message.template_id = template_id
+
+        sg = SendGridAPIClient(sendgrid_api_key)
+        response = sg.send(email_message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
