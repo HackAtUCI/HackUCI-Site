620ae504df53248587edbb5c437c425e0990606c
Riley Wong <rileysw@uci.edu>
2022-12-27 13:30:19 -0800

    updated formatting

--- src/api/sendgrid/sendgrid_email.py
+++ src/api/sendgrid/sendgrid_email.py
@@ -1,33 +1,49 @@
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
 import os
+
 import aiosendgrid
+
 from sendgrid.helpers.mail import Email, Mail, Personalization
 
-SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
 
-async def send_email(template_id: str, sender_email: str, receiver_data: dict or list, send_to_multiple: bool = False) -> None:
-    '''
+async def send_email(
+    template_id: str,
+    sender_email: str,
+    receiver_data: dict or list,
+    send_to_multiple: bool = False,
+) -> None:
+    """
     Send a personalized templated email to one or multiple receivers via SendGrid
-    '''
+    """
     try:
         email_message = Mail()
 
         if send_to_multiple:
             if type(receiver_data) != list:
-                raise TypeError(f'receiver_data expects {list}. Passed object has type {type(receiver_data)}')
+                raise TypeError(
+                    f"receiver_data expects {list}. Passed object has type {type(receiver_data)}"
+                )
             else:
                 for r in receiver_data:
                     p = Personalization()
-                    p.add_to(Email(email=r['email'], dynamic_template_data=r))
+                    p.add_to(Email(email=r["email"], dynamic_template_data=r))
                     email_message.add_personalization(p)
         else:
             if type(receiver_data) != dict:
-                raise TypeError(f'receiver_data expects {dict}. Passed object has type {type(receiver_data)}')
+                raise TypeError(
+                    f"receiver_data expects {dict}. Passed object has type {type(receiver_data)}"
+                )
             else:
                 p = Personalization()
-                p.add_to(Email(email=receiver_data['email'], dynamic_template_data=receiver_data))
+                p.add_to(
+                    Email(
+                        email=receiver_data["email"],
+                        dynamic_template_data=receiver_data,
+                    )
+                )
                 email_message.add_personalization(p)
 
         email_message.from_email = sender_email
