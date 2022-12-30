645339fb968e67e0f959fdb9d506c5e5af74b299
Sam Der <sder@uci.edu>
2022-12-30 10:15:58 -0800

    Add FAQ accordion with placeholder questions

--- src/views/home/sections/Faq.tsx
+++ src/views/home/sections/Faq.tsx
@@ -1,12 +1,22 @@
-import Container from "react-bootstrap/Container";
+import questions from 'assets/data/faq.json';
+import Accordion from 'react-bootstrap/Accordion';
 
 function Faq() {
-	return (
-		<Container as="section">
-			<h2>FAQ</h2>
-			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-		</Container>
-	);
+    return (
+        <section className="faq-block">
+            <h2>FAQ</h2>
+            <Accordion>
+                {questions.map((q, index) => (
+                    <Accordion.Item key={index} eventKey={`${index}`}>
+                        <Accordion.Header>{q["question"]}</Accordion.Header>
+                        <Accordion.Body>
+                            {q["answer"]}
+                        </Accordion.Body>
+                    </Accordion.Item>
+                ))}
+            </Accordion>
+        </section>
+    )
 }
 
 export default Faq;
