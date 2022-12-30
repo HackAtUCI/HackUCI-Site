c0833cca0231efafa6447dbf97b7f564dae0d180
Taesung Hwang <taesungh@uci.edu>
2022-12-29 23:48:52 -0800

    Update theme colors/fonts and add placeholder text
    - Update theme colors and fonts as graphics adjusted the design
    - Create placeholder sections to visually check theme

---
+++ src/views/home/sections/Faq.tsx
@@ -0,0 +1,12 @@
+import Container from "react-bootstrap/Container";
+
+function Faq() {
+	return (
+		<Container as="section">
+			<h2>FAQ</h2>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+		</Container>
+	);
+}
+
+export default Faq;
