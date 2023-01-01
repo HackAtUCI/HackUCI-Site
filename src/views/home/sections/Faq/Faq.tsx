5a6bf8106ff74b9380bf6772fc7791149a33735e
Sam Der <sder@uci.edu>
2022-12-31 18:47:31 -0800

    Override Bootstrap color and padding settings

--- src/views/home/sections/Faq/Faq.tsx
+++ src/views/home/sections/Faq/Faq.tsx
@@ -6,11 +6,11 @@ function Faq() {
     return (
         <section className="container faq-block">
             <h2 className="text-center">FAQ</h2>
-            <Accordion defaultActiveKey="0">
+            <Accordion defaultActiveKey="0" className={styles['accordion']}>
                 {questions.map((q, index) => (
-                    <Accordion.Item key={index} eventKey={`${index}`}>
-                        <Accordion.Header className={styles['accordion-border']}>{q["question"]}</Accordion.Header>
-                        <Accordion.Body className={styles['accordion-border']}>
+                    <Accordion.Item key={index} className={styles['accordion-border']} eventKey={`${index}`}>
+                        <Accordion.Header>{q["question"]}</Accordion.Header>
+                        <Accordion.Body>
                             {q["answer"]}
                         </Accordion.Body>
                     </Accordion.Item>
