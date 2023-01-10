2d3e15690a312ed8c08d91d856a795ca9f130bbb
Riley Wong <rileysw@uci.edu>
2023-01-10 09:19:33 -0800

    added application links and buttons

--- src/views/home/sections/MentorsVolunteers/MentorsVolunteers.tsx
+++ src/views/home/sections/MentorsVolunteers/MentorsVolunteers.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
@@ -14,26 +15,45 @@ function MentorsVolunteers() {
 				<div className={styles.statueContainer}>
 					<Image src={mentorStatue} alt="HackUCI Mentor Statue" />
 					<h3 className={styles.statueTitle}>Apply to be a Mentor</h3>
-					<p className={styles.statueDescription}>
-						Have a knack for innovation? Interested in inspiring the next
-						generation of developers? Our mentors are vital to making HackUCI
-						2023 successful and enjoyable for our hackers. Apply to be a mentor
-						today!
-					</p>
-					{/*TODO: Apply Button here*/}
+					<div className={styles.statueDescription}>
+						<p>
+							Have a knack for innovation? Interested in inspiring the next
+							generation of developers? Our mentors are vital to making HackUCI
+							2023 successful and enjoyable for our hackers. Apply to be a
+							mentor today!
+						</p>
+						<Button
+							variant="museum"
+							href="https://forms.gle/tcnikpj5gHnGPNvx7"
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							Mentor Application
+						</Button>
+					</div>
 				</div>
 			</Col>
 			<Col>
 				<div className={styles.statueContainer}>
 					<Image src={volunteerStatue} alt="HackUCI Volunteer Statue" />
 					<h3 className={styles.statueTitle}>Become a Volunteer</h3>
-					<p className={styles.statueDescription}>
-						Want a peek behind the scenes? Join our team of event volunteers
-						that make HackUCI run as smoothly as possible! Many roles are
-						available and shifts are flexible. Plus, get access to all the free
-						food and all our hackathon swag. We hope to see you on the team!
-					</p>
-					{/*TODO: Apply Button here*/}
+					<div className={styles.statueDescription}>
+						<p>
+							Want a peek behind the scenes? Join our team of event volunteers
+							that make HackUCI run as smoothly as possible! Many roles are
+							available and shifts are flexible. Plus, get access to all the
+							free food and all our hackathon swag. We hope to see you on the
+							team!
+						</p>
+						<Button
+							variant="museum"
+							href="https://forms.gle/bLw9nHffqoz4kAGR7"
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							Volunteer Form
+						</Button>
+					</div>
 				</div>
 			</Col>
 		</Row>
