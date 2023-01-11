520965f4f4fae6d2d32d422a12a4dc8e55b98dd0
Jason Zenarosa <jasonzenarosa@gmail.com>
2023-01-10 19:10:32 -0800

    [#18] Added text content to intro section

---
+++ src/views/home/sections/WallPanel/WallPanel.tsx
@@ -0,0 +1,21 @@
+function WallPanel() {
+    return (
+        <div>
+            <div className="intro-section">
+                <h2>36 Hours</h2>
+                <h2>500+ Hackers</h2>
+                <h2>$10,000 in Prizes</h2>
+                <h3>Create + Connect + Inspire</h3>
+                <p>
+                    HackUCI is the largest collegiate hackathon in Orange County and we continue expanding and improving our event every year. Our focus? - Enhance the community around us by giving students the platform to unleash their creativity in an environment of forward thinking individuals.
+                </p>
+                <p>
+                    This year, HackUCI will take place the weekend of February 3rd to 5th. The event will be 100% in-person during the day (not overnight). Free workshops, socials, food, and swag will be provided!
+                </p>
+            </div>
+            <div className="artwork"></div>
+        </div>
+    );
+}
+
+export default WallPanel;
