4ca434e9cf40404fc621d2fe5e94f1da4a03a811
Randy Huynh <randyhuynh5815@gmail.com>
2023-01-11 23:23:16 -0800

    Added button styling and hrefs

--- src/views/home/sections/Landing.tsx
+++ src/views/home/sections/Landing.tsx
@@ -1,27 +1,38 @@
-import benchImage from 'assets/images/bench_and_viewers.svg';
-import TextPanel from 'components/TextPanel/TextPanel';
-import Image from 'next/image';
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import styles from './Landing.module.scss';
-
+import benchImage from "assets/images/bench_and_viewers.svg";
+import TextPanel from "components/TextPanel/TextPanel";
+import Image from "next/image";
+import Button from "react-bootstrap/Button";
+import styles from "./Landing.module.scss";
 
 function Landing() {
-	const panelContent = <div>
-		<div className={styles.homeDate}>Feburary 3 - 5, 2023</div>
-		<div className={styles.homeHeading}>HackUCI</div>
+	const panelContent = (
+		<div>
+			<div className={styles.homeDate}>Feburary 3 - 5, 2023</div>
+			<div className={styles.homeHeading}>HackUCI</div>
 
-		<div className={styles.homeButtons}>
-			<Button className="btn-museum btn-landing" href="#">Apply as a Hacker</Button>
-			<Button className="btn-museum btn-landing" href="#">Apply as a Mentor</Button>
+			<div className={styles.homeButtons}>
+				<Button className="btn-museum btn-landing" href="/apply">
+					Apply as a Hacker
+				</Button>
+				<Button
+					className="btn-museum btn-landing"
+					href="https://forms.gle/tcnikpj5gHnGPNvx7"
+				>
+					Apply as a Mentor
+				</Button>
+			</div>
 		</div>
-	</div>;
+	);
 
 	return (
 		<section className={styles.homeLanding}>
 			<TextPanel props={panelContent} />
 			<div className={styles.homeBenches}>
-				<Image src={benchImage} alt="Landing Benches" className={styles.homeBenchImage} />
+				<Image
+					src={benchImage}
+					alt="Landing Benches"
+					className={styles.homeBenchImage}
+				/>
 			</div>
 		</section>
 	);
