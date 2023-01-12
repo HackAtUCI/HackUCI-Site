e9b3dc8159e0ccdedecc122119171ce4b87e9f0e
Taesung Hwang <taesungh@uci.edu>
2023-01-11 17:52:08 -0800

    Add visually hidden <h2> for Support section

--- src/views/home/sections/Support.tsx
+++ src/views/home/sections/Support.tsx
@@ -5,6 +5,7 @@ import MentorsVolunteers from "./MentorsVolunteers/MentorsVolunteers";
 function Support() {
 	return (
 		<Container as="section">
+			<h2 className="visually-hidden">Support</h2>
 			<MentorsVolunteers />
 		</Container>
 	);
