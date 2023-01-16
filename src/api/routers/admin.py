bc873bbcad264e4fb5c3f1e0c7ea4e42350b9d76
Sam Der <samder713@gmail.com>
2023-01-15 16:52:15 -0800

    Optimize checking whether a user is authorized

--- src/api/routers/admin.py
+++ src/api/routers/admin.py
@@ -10,11 +10,15 @@ log = getLogger(__name__)
 router = APIRouter()
 
 
-async def _get_administrators() -> set[object]:
+async def _is_not_authorized(
+    user: User = Depends(require_user_identity),
+) -> bool:
+    if not user:
+        return True
     response: list[dict[str, object]] = await retrieve(
-        Collection.USERS, {"role": "admin"}, ["_id"]
+        Collection.USERS, {"role": "admin", "_id": user.uid}
     )
-    return set(i["_id"] for i in response)
+    return len(response) > 0
 
 
 @router.get("/users", status_code=status.HTTP_200_OK)
@@ -22,9 +26,11 @@ async def users(
     response: Response, user: User = Depends(require_user_identity)
 ) -> list[dict[str, object]]:
     log.info(user)
-    if not user or user.uid not in await _get_administrators():
+
+    if _is_not_authorized(user):
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return []
+
     result: list[dict[str, object]] = await retrieve(
         Collection.USERS, {"role": "applicant"}, []
     )
