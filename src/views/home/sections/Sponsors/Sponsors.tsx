460ab4b2419a190ba5390d35559132b660286b64
Pegysus <maxyeh@gmail.com>
2023-01-17 08:48:05 -0800

    center aligned image, added sponsor text, and fixed a typo

--- src/views/home/sections/Sponsors/Sponsors.tsx
+++ src/views/home/sections/Sponsors/Sponsors.tsx
@@ -1,4 +1,4 @@
-import sponsors from 'assets/data/sponsers.json';
+import sponsors from 'assets/data/sponsors.json';
 import stickermuleSrc from 'assets/sponsors/stickermule.svg';
 import Image from 'next/image';
 import React from 'react';
@@ -12,6 +12,8 @@ function Sponsors() {
     return (
         <section id="sponsors" className="container sponsors-block">
             <h2 className="text-center">Sponsors</h2>
+            <div className={styles.sponsorInfo}>Interested in sponsoring HackUCI 2023? Check out our information above to
+                                                learn more about our event! For more information, please email us at hack@uci.edu.</div>
             <div className={styles.sponsorImages}>
                 <a key={stickermule.name} href={stickermule.url} className={styles.sponsorLink}>
                     <Image src={stickermuleSrc} alt={stickermule.name} className={styles.sponsorImage} />
