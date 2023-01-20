97630eaba6edb44c2ca4a075e01827ed7be2905f
Sam Der <samder713@gmail.com>
2023-01-19 20:24:08 -0800

    Change instances of 'HackUCI' to 'Hack at UCI'

--- src/views/apply/components/ApplicationPreface.tsx
+++ src/views/apply/components/ApplicationPreface.tsx
@@ -10,11 +10,11 @@ function ApplicationPreface(props: ApplicationPrefaceProps) {
 		<div>
 			<h2>Before Applying</h2>
 			<p>
-				By submitting an application for HackUCI 2023, I understand that HackUCI
-				will take place in person during the day from February 3rd to 5th, and
-				that HackUCI will not be providing transportation or overnight
-				accommodations. In addition, I understand that I must check in at
-				certain times on all three event days in order to be eligible to win
+				By submitting an application for Hack at UCI 2023, I understand that
+				Hack at UCI will take place in person during the day from February 3rd
+				to 5th, and that Hack at UCI will not be providing transportation or
+				overnight accommodations. In addition, I understand that I must check in
+				at certain times on all three event days in order to be eligible to win
 				prizes. Lastly, I acknowledge that I am currently a student enrolled in
 				an accredited college or university in the United States and will be
 				over the age of 18 by February 3rd, 2023.
