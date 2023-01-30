fa8c7a3aee04ae31f687d6790357215cdecb0bde
Riley Wong <rileysw@uci.edu>
2023-01-29 22:35:29 -0800

    updated portal message content

--- src/views/portal/AcceptedMessage.tsx
+++ src/views/portal/AcceptedMessage.tsx
@@ -12,12 +12,7 @@ function AcceptedMessage() {
 				and sign the Event Waiver prior to the event. You will receive the
 				waiver via email closer to the event date.{" "}
 				<strong className={styles.red_text}>
-					<i>
-						<u>
-							You must complete the waiver in order to participate in Hack at
-							UCI.
-						</u>
-					</i>
+					You must complete the waiver in order to participate in Hack at UCI.
 				</strong>
 			</p>
 
@@ -28,12 +23,15 @@ function AcceptedMessage() {
 			</p>
 
 			<h4>Team Formation</h4>
+			<p>Still looking for teammates? Attend our two team formation events!</p>
+			<ol>
+				<li>Thursday (2/2) 7-8 PM at DBH 6011</li>
+				<li>Friday (2/3) 7-8 PM at Student Center Pacific Ballroom</li>
+			</ol>
 			<p>
-				Still looking for teammates? Attend our two team formation events,
-				Thursday (2/2) 7-8 PM in DBH 6011 and Friday (2/3) 7-8 PM in Student
-				Center Pacific Ballroom. You can also create an introductory slide{" "}
+				You can also create an introductory slide on our{" "}
 				<a href="https://docs.google.com/presentation/d/1qjAzK9ocrI5Lg2D8l4VnE38CWDU2bW2v3XqAROA-vR4/edit#slide=id.g1ecf0247681_1_277">
-					here
+					team formation slides
 				</a>{" "}
 				to connect with other hackers or use the #team-formation channel in
 				Slack to find teammates! Teams are limited to groups of up to four.
@@ -44,7 +42,7 @@ function AcceptedMessage() {
 				Please be mindful of your personal health and of those around you.
 				Although not required, we strongly encourage wearing masks, and please
 				stay home if you feel sick. To help us ensure your safety at the venue,
-				please keep your <strong>badge</strong> (issued at check in){" "}
+				please keep your <strong>badge</strong> (issued at check-in){" "}
 				<strong>visible at all times</strong>.
 			</p>
 
@@ -61,25 +59,25 @@ function AcceptedMessage() {
 				</strong>
 				. Hackathon events will take place in the following rooms:
 			</p>
+			<ul style={{ listStyleType: "none", paddingLeft: 0 }}>
+				<strong>Pacific Ballroom Lobby:</strong> Check-In
+				<li>
+					<strong>Pacific Ballroom ABCD:</strong> Hacking Space
+				</li>
+				<li>
+					<strong>Woods Cove B + C:</strong> Hacking Space
+				</li>
+				<li>
+					<strong>Moss Cove A + B:</strong> Workshops and Socials
+				</li>
+			</ul>
 			<p>
-				<strong>Pacific Ballroom Lobby:</strong> Check In
-				<br />
-				<strong>Pacific Ballroom ABCD:</strong> Hacking Space
-				<br />
-				<strong>Woods Cove B + C:</strong> Hacking Space
-				<br />
-				<strong>Moss Cove A + B:</strong> Workshops and Socials
-			</p>
-			<p>
-				Map:{" "}
 				<a href="https://map.uci.edu/?id=463#!m/255880">
-					https://map.uci.edu/?id=463#!m/255880
+					Map to UCI Student Center
 				</a>
-			</p>
-			<p>
-				Floor Map:{" "}
+				<br />
 				<a href="https://conference.studentaffairs.uci.edu/node/24">
-					https://conference.studentaffairs.uci.edu/node/24
+					Student Center Floor Map
 				</a>
 			</p>
 			<h4>On-Campus Parking</h4>
@@ -87,9 +85,9 @@ function AcceptedMessage() {
 				<strong>Attendees will be responsible for their own parking.</strong>{" "}
 				The closest parking structure to our event is the UCI Student Center
 				Parking Structure. This structure provides both pay-by-space ($2 per
-				hour) and permit dispensers ($1 per hour, $13 for a day). You may
+				hour) and permit dispensers ($1 per hour or $13 per day). You may
 				purchase a permit at any information kiosk, dispenser, or through
-				UCI&#39;s ParkbyPlate signage located in select parking areas. All
+				UCI&apos;s ParkbyPlate signage located in select parking areas. All
 				options accept major credit cards (Visa, MasterCard, American Express,
 				Discover). These dispensers are available from 7AM-7PM.
 			</p>
@@ -103,9 +101,8 @@ function AcceptedMessage() {
 				via Slack, and we&#39;d be happy to help.
 			</p>
 			<p>
-				Map:{" "}
 				<a href="https://map.concept3d.com/?id=463#!ce/8403?ct/55161,27429,35849?m/367524?s/">
-					https://map.concept3d.com/?id=463#!ce/8403?ct/55161,27429,35849?m/367524?s/
+					Map to UCI Student Center Parking Structure
 				</a>
 			</p>
 
@@ -118,7 +115,7 @@ function AcceptedMessage() {
 
 			<p>
 				<u>Friday (2/3):</u> <br />
-				5:00 PM - Venue Opens for Day 1 Check In <br />
+				5:00 PM - Venue Opens for Day 1 Check-In <br />
 				6:00 PM - Dinner <br />
 				7:00 PM - Team Formation #2 <br />
 				8:00 PM - Opening Ceremony <br />
@@ -128,7 +125,7 @@ function AcceptedMessage() {
 
 			<p>
 				<u>Saturday (2/4):</u> <br />
-				8:00 AM - Venue Opens for Day 2 Check In <br />
+				8:00 AM - Venue Opens for Day 2 Check-In <br />
 				9:00 AM - Breakfast <br />
 				12:00 PM - Lunch <br />
 				6:00 PM - Dinner <br />
@@ -137,7 +134,7 @@ function AcceptedMessage() {
 
 			<p>
 				<u>Sunday (2/5):</u> <br />
-				7:00 AM - Venue Opens for Day 3 Check In <br />
+				7:00 AM - Venue Opens for Day 3 Check-In <br />
 				<strong> 9:00 AM - Hacking Ends</strong> <br />
 				9:00 AM - Breakfast <br />
 				9:30 AM - Project Expo / Judging <br />
@@ -145,23 +142,23 @@ function AcceptedMessage() {
 			</p>
 
 			<h4>Pre-Hack-At-UCI Checklist</h4>
-			<ul>
+			<ol>
 				<li>Confirm your attendance by clicking on the button below</li>
 				<li>Sign the liability waiver once it has been sent to your email</li>
 				<li>Join the Slack once the invite has been sent to your email</li>
 				<li>
-					Bring a <strong>photo ID for check</strong> in on Day 1, and your{" "}
+					Bring a <strong>photo ID</strong> for check-in on Day 1, and your{" "}
 					<strong>badge</strong> to check in on Day 2 and 3. Other items to
 					bring include a laptop + charger, refillable water bottle, and a
 					positive attitude to the event!
 				</li>
-			</ul>
+			</ol>
 
 			<h4>Questions/Comments</h4>
 			<p>
 				If you have any questions that have not yet been answered, please reach
-				out to us at <u>hack@uci.edu</u>, and we will be glad to assist you with
-				your concerns.
+				out to us at <a href="mailto:hack@uci.edu">hack@uci.edu</a>, and we will
+				be glad to assist you with your concerns.
 			</p>
 			<p>
 				Best Regards, <br />
