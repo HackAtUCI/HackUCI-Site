8ed44e5ca72105971a3e2934d7f43b24da8c20d9
Nathan Huey <njhuey@uci.edu>
2023-01-30 21:51:03 -0800

    added rsvp_component to VerticalTimeline

--- src/views/portal/VerticalTimeline.tsx
+++ src/views/portal/VerticalTimeline.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import ListGroup from "react-bootstrap/ListGroup";
 
+import BorderCircle from "assets/icons/border-circle.svg";
 import CheckCircle from "assets/icons/check-circle-fill.svg";
 import XCircle from "assets/icons/x-circle-fill.svg";
 
@@ -51,11 +52,37 @@ function VerticalTimeline({ status }: VerticalTimelineProps) {
 			</ListGroup.Item>
 		) : null;
 
+	const rsvp_component =
+		status === PortalStatus.accepted ? (
+			<ListGroup.Item className={styles.list_item}>
+				<Image
+					src={BorderCircle}
+					alt="border-circle"
+					width={25}
+					height={25}
+					className={styles.icon}
+				/>
+				Confirm attendance
+			</ListGroup.Item>
+		) : status === PortalStatus.confirmed ? (
+			<ListGroup.Item className={styles.list_item}>
+				<Image
+					src={CheckCircle}
+					alt="checked-circle"
+					width={25}
+					height={25}
+					className={styles.icon}
+				/>
+				Attendance confirmed
+			</ListGroup.Item>
+		) : null;
+
 	return (
 		<div className="p-3">
 			<ListGroup className={styles.list}>
 				{submission_component}
 				{verdict_component}
+				{rsvp_component}
 			</ListGroup>
 		</div>
 	);
