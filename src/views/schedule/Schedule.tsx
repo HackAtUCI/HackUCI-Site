9d0ac673cb0b81de957a3d389e4e02a73397c761
Pegysus <maxyeh@gmail.com>
2023-02-02 00:17:11 -0800

    added Countdown component to banner. It is a bit jank right now but I will try to fix css/sizing later if I have time (if anyone has time to help fix that'll be greatly appreciated)

--- src/views/schedule/Schedule.tsx
+++ src/views/schedule/Schedule.tsx
@@ -3,12 +3,43 @@ import { TitleBanner } from "components";
 import Container from "react-bootstrap/Container";
 import Announcement from "views/schedule/components/Announcement";
 import Event from "views/schedule/components/Event";
+import Countdown from "./components/Countdown";
+import './Schedule.module.scss';
 
 function Schedule() {
+	const hackingBegins = "3 Feb 2023 21:00:00 PDT";
+	const devpostSubmission = "5 Feb 2023 9:00:00 PDT";
+
+	function generateCountdown() {
+		if (Date.parse(hackingBegins) > Date.now()) {
+			return (
+				<section id="schedule-block">
+					<div className="schedule-countdown text-center">
+						<Countdown date={hackingBegins} />
+					</div>
+					<div className="schedule-countdown-text">
+						<span> Until Hacking Begins</span>
+					</div>
+				</section>
+			);
+		} else {
+			return (
+				<section id="schedule-block">
+					<div className="schedule-countdown text-center">
+						<Countdown date={devpostSubmission} />
+					</div>
+					<div className="schedule-countdown-text">
+						<span> Until Devpost Submission Closes</span>
+					</div>
+				</section>
+			);
+		}
+	}
+
 	return (
 		<Container>
 			<TitleBanner>
-				<h1>Schedule</h1>
+				{generateCountdown()}
 			</TitleBanner>
 			{schedule.friday.map((event) =>
 				event.category === "Announcement" ? (
---
+++ src/views/schedule/components/Countdown.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Confetti from 'react-confetti';
+import FlipNumbers from 'react-flip-numbers';
+
+function Countdown(props: any) {
+
+    const ref = useRef<any | null>(null);
+
+    const [timer, setTimer] = useState('00:00:00:00');
+    const [hasTimerInit, setHasTimerInit] = useState(false);
+
+    const getWinDims = () => {
+        const { innerWidth: width, innerHeight: height } = window;
+        return { width, height };
+    }
+
+    const [winDim, setWinDim] = useState(getWinDims());
+
+    useEffect(() => {
+        const remainingTime = () => {
+            const total = Date.parse(props.date) - Date.now()
+            const seconds = Math.floor((total / 1000) % 60)
+            const minutes = Math.floor((total / 1000 / 60) % 60)
+            const hours = Math.floor((total / 1000 / 60 / 60) % 24)
+            const days = Math.floor((total / 1000 / 60 / 60 / 24))
+            return { total, seconds, minutes, hours, days };
+        }
+
+        const startTime = () => {
+            let { total, seconds, minutes, hours, days } = remainingTime();
+            if (total >= 0) {
+                setTimer(
+                    days.toString().padStart(2, '0') + ':' + hours.toString().padStart(2, '0') + ':' +
+                    minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0')
+                )
+            }
+            setHasTimerInit(true);
+        }
+
+        if (ref.current) clearInterval(ref.current);
+
+        const id = setInterval(() => {
+            startTime();
+        }, 1000);
+        ref.current = id;
+
+    }, [props.date]);
+
+    useEffect(() => {
+        function handleResize() {
+            setWinDim(getWinDims());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    const countdownGenerator = () => {
+        if (winDim.width > 900) {
+            if (hasTimerInit && timer === '00:00:00:00') {
+                return (
+                    <>
+                        <Confetti width={110 * 8 + 50} />
+                        <FlipNumbers play numbers={timer} color="white" width={50} height={70} numberStyles={{ textAlign: "left", fontSize: "95px" }} />
+                    </>
+                )
+            }
+            return (<FlipNumbers play numbers={timer} color="white" width={50} height={70} numberStyles={{ textAlign: "left", fontSize: "95px" }} />);
+        } else {
+            if (hasTimerInit && timer === '00:00:00:00') {
+                return (
+                    <>
+                        <Confetti width={30 * 8 + 30} />
+                        <FlipNumbers play numbers={timer} color="white" width={30} height={50} numberStyles={{ textAlign: "left", fontSize: "45px" }} />
+                    </>
+                )
+            }
+            return (<FlipNumbers play numbers={timer} color="white" width={30} height={50} numberStyles={{ textAlign: "left", fontSize: "45px" }} />);
+        }
+    }
+
+    return countdownGenerator();
+
+}
+
+export default Countdown;
