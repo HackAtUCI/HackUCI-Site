cff1434dd260846b35cbe78f5eb73ae0a7f73c3c
Riley Wong <rileysw@uci.edu>
2023-02-05 11:51:04 -0800

    fix eslinting errors

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -55,9 +55,7 @@ function Navigation() {
 						</PrivateNavLinkItem>
 						<NavLinkItem href="/resources">Resources</NavLinkItem>
 						<NavLinkItem href="/feedback">Feedback</NavLinkItem>
-						<NavLinkItem href="/devpost">
-							Devpost
-						</NavLinkItem>
+						<NavLinkItem href="/devpost">Devpost</NavLinkItem>
 						<NavLinkItem href="/report">Report Incident</NavLinkItem>
 						<NavLinkItem
 							href={logButtonPath}
